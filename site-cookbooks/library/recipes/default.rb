68265079a1e4b682d847baa96a50a2b4a22c0a50
Takatoshi Maeda <maeda@labit.com>
2013-03-15 17:52:46 +0900

    add homebrew installed mysql initialize process

--- site-cookbooks/library/recipes/default.rb
+++ site-cookbooks/library/recipes/default.rb
@@ -1,18 +1,20 @@
 include_recipe 'homebrew'
 
 node.packages.each do |prov, values|
-  prov = if prov == "homebrew"
-           Chef::Provider::Package::Homebrew
-         elsif prov == "rpm"
-           Chef::Provider::Package::Rpm
-         elsif prov == "yum"
-           Chef::Provider::Package::Yum
-         end
-  values.each do |pkg|
-    package (pkg.class == String ? pkg : pkg["name"]) do
+  provider_class = if prov == "homebrew"
+                     Chef::Provider::Package::Homebrew
+                   elsif prov == "rpm"
+                     Chef::Provider::Package::Rpm
+                   elsif prov == "yum"
+                     Chef::Provider::Package::Yum
+                   end
+  values.items.each do |pkg|
+    name = (pkg.class == String ? pkg : pkg["name"])
+    package name do
       action :install
-      provider prov
-      source pkg["source"] if prov == Chef::Provider::Package::Rpm
+      provider provider_class
+      source pkg["source"] if provider_class == Chef::Provider::Package::Rpm
     end
+    send("initialize_#{prov}_#{name}") if values.initialize.include?(name)
   end
 end
