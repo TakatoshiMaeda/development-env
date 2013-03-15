8161daf381804a2e2ca265c8025dca7236578025
Takatoshi Maeda <maeda@labit.com>
2013-03-15 16:59:54 +0900

    change base directory

--- solo.rb
+++ solo.rb
@@ -1,6 +1,6 @@
-file_cache_path           "/tmp/chef-solo/cache"
-data_bag_path             "/tmp/chef-solo/data_bags"
-encrypted_data_bag_secret "/tmp/chef-solo/data_bag_key"
-cookbook_path             [ "/tmp/chef-solo/site-cookbooks",
-                            "/tmp/chef-solo/cookbooks" ]
-role_path                 "/tmp/chef-solo/roles"
+file_cache_path           "#{`echo $HOME`.gsub("\n", "")}/config/chef-solo/cache"
+data_bag_path             "#{`echo $HOME`.gsub("\n", "")}/config/chef-solo/data_bags"
+encrypted_data_bag_secret "#{`echo $HOME`.gsub("\n", "")}/config/chef-solo/data_bag_key"
+cookbook_path             [ "#{`echo $HOME`.gsub("\n", "")}/config/chef-solo/site-cookbooks",
+                            "#{`echo $HOME`.gsub("\n", "")}/config/chef-solo/cookbooks" ]
+role_path                 "#{`echo $HOME`.gsub("\n", "")}/config/chef-solo/roles"
