cc0a5028f6b7e0f1704eeae1f10ab5421941a5b2
Takatoshi Maeda <maeda@labit.com>
2013-04-07 07:27:14 +0900

    library cookbooks move

--- roles/dev_common.rb
+++ roles/dev_common.rb
@@ -3,7 +3,7 @@ description 'common environment for development user'
 run_list 'recipe[macapps::bettertouchtool]', 'recipe[macapps::elasticfox]', 'recipe[macapps::keyremap4macbook]', 'recipe[macapps::mou]',
   'recipe[macapps::sublimetext2]', 'recipe[macapps::totalterminal]', 'recipe[macapps::virtualbox]', 'recipe[macapps::virtualbox]',
   'recipe[macapps::sequelpro]', 'recipe[macapps::vagrant]', 'recipe[myapps::vmware_fusion]', 'recipe[macapps::heroku]', 'recipe[macapps::pivotal_booster]',
-  'recipe[myapps::rubymotion]', 'recipe[macapps::cyberduck]', 'recipe[library::powerline]'
+  'recipe[myapps::rubymotion]', 'recipe[macapps::cyberduck]', 'recipe[mylibrary::powerline]'
 default_attributes(
   :packages => {
     :items => [
