b0fb5d9c9c6a94986a02b78cae8ae452586887d2
Takatoshi Maeda <maeda@labit.com>
2013-04-13 17:38:35 +0900

    modified roles

--- roles/dev_common.rb
+++ roles/dev_common.rb
@@ -3,21 +3,4 @@ description 'common environment for development user'
 run_list 'recipe[macapps::bettertouchtool]', 'recipe[macapps::elasticfox]', 'recipe[macapps::keyremap4macbook]', 'recipe[macapps::mou]',
   'recipe[macapps::sublimetext2]', 'recipe[macapps::totalterminal]', 'recipe[macapps::virtualbox]', 'recipe[macapps::virtualbox]',
   'recipe[macapps::sequelpro]', 'recipe[macapps::vagrant]', 'recipe[myapps::vmware_fusion]', 'recipe[macapps::heroku]', 'recipe[macapps::pivotal_booster]',
-  'recipe[myapps::rubymotion]', 'recipe[macapps::cyberduck]', 'recipe[mylibrary::powerline]'
-default_attributes(
-  :packages => {
-    :items => [
-      "git",
-      "tmux",
-      "zsh",
-      "tree",
-      "wget",
-      "readline",
-      "htop-osx",
-      "git-flow",
-      "csshx",
-      "hub",
-      "jq"
-    ]
-  }
-)
+  'recipe[macapps::xquartz]', 'recipe[myapps::rubymotion]', 'recipe[macapps::cyberduck]', 'recipe[mylibrary::powerline]'
--- roles/infrastructure.rb
+++
@@ -1,9 +0,0 @@
-name 'infrastructure'
-description 'environment for infrastructure development user'
-default_attributes(
-  :packages => {
-    :items => [
-      "tiger-vnc"
-    ]
-  }
-)
